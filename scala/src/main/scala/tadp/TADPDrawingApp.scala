9dbe594e6183b61fcbe1006085d8fe1ebd977e73
pablitar <pablitar@gmail.com>
2020-10-03 18:22:03 -0300

    Adaptando la parte de Scala para el TP

---
+++ scala/src/main/scala/tadp/TADPDrawingApp.scala
@@ -0,0 +1,13 @@
+package tadp
+
+import scalafx.scene.paint.{Color => C}
+
+object TADPDrawingApp extends App {
+  TADPDrawingAdapter
+    .forImage("test.png") { adapter =>
+      adapter
+        .beginColor(C.rgb(100, 100, 100))
+          .rectangle((200, 200), (400, 400))
+        .end()
+    }
+}
