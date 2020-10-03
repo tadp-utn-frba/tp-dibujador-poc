a49650f205f069e9a65e22399c118a7f52a7156c
pablitar <pablitar@gmail.com>
2020-10-03 18:23:33 -0300

    Agregado ejemplo de usar forScreen

--- scala/src/main/scala/tadp/TADPDrawingApp.scala
+++ scala/src/main/scala/tadp/TADPDrawingApp.scala
@@ -4,10 +4,18 @@ import scalafx.scene.paint.{Color => C}
 
 object TADPDrawingApp extends App {
   TADPDrawingAdapter
-    .forImage("test.png") { adapter =>
+    .forScreen { adapter =>
       adapter
         .beginColor(C.rgb(100, 100, 100))
-          .rectangle((200, 200), (400, 400))
+        .rectangle((200, 200), (400, 400))
         .end()
     }
+
+  //  TADPDrawingAdapter
+  //    .forImage("test.png") { adapter =>
+  //      adapter
+  //        .beginColor(C.rgb(100, 100, 100))
+  //        .rectangle((200, 200), (400, 400))
+  //        .end()
+  //    }
 }
